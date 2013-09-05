7c7f22c322cb0b816dbab41a861d984e440c208b
jbc22 <brettcu@gmail.com>
2013-09-05 19:46:14 -0400

    remove nested loop

--- allsum-nsrl.rb
+++ allsum-nsrl.rb
@@ -5,8 +5,8 @@ require 'data_mapper'
 require 'csv'
 
 db = DataMapper.setup(:default, 'mysql://root:@localhost/hashdb')
-
-class File
+               
+class Record
   include DataMapper::Resource
 
   property :id,           Serial
@@ -17,21 +17,39 @@ class File
   property :filesize,     Integer
   property :productcode,  Integer
   property :opsystemcode, String
-  property :specialcode,  String 
+  property :specialcode,  String
+  property :filepath,     Text
 end
-
 
 DataMapper.finalize
-DataMapper.auto_migrate!
+#DataMapper.auto_migrate!
 #DataMapper.auto_upgrade!
 
-#nsrlfile = CSV.foreach(ARGV[1]) do |row|
-CSV.foreach(ARGV[0], :headers => true) do |row|
-  p row
-  puts row[0].inspect
-  #<CSV::Row "SHA-1":"000000206738748EDD92C4E3D2E823896700F849" "MD5":"392126E756571EBF112CB1C1CDEDF926" "CRC32":"EBD105A0" "FileName":"I05002T2.PFB" "FileSize":"98865" "ProductCode":"3095" "OpSystemCode":"WIN" "SpecialCode":"">
-  File.create(:sha1 => row[0]) #, :md5 => row[2], :crc32 => row[3], :filename => row[4], :filesize => row[5], :productcode => row[6], :opsystemcode => row[7], :specialcode => row[8]
+#CSV.foreach(ARGV[0], {:encoding => "r:ISO-8859-15:UTF-8", :headers => true}) do |row|
+#  #<CSV::Row "SHA-1":"000000206738748EDD92C4E3D2E823896700F849" "MD5":"392126E756571EBF112CB1C1CDEDF926" "CRC32":"EBD105A0" "FileName":"I05002T2.PFB" "FileSize":"98865" "ProductCode":"3095" "OpSystemCode":"WIN" "SpecialCode":"">
+#  file = Record.create(:sha1 => row[0], :md5 => row[1], :crc32 => row[2], :filename => row[3], :filesize => row[4], :productcode => row[5], :opsystemcode => row[6], :specialcode => row[7])
+#  puts file
+#end
+
+#CSV.foreach(ARGV[0], :headers => true) do |row|
+
+#paths = Hash[ CSV.read('filepath.csv', "r:ISO-8859-15:UTF-8").map do |row|
+#  puts [ row[0].to_i, [row[1].strip] ]
+#end ]
+h = Hash.new
+paths = CSV.read("filepath-short.csv", "r:ISO-8859-15:UTF-8").each_with_object({}) do |k, v|
+  #h{:sha1 = v} = v
+  #puts h[1].inspect
+  #puts k[0]
+  #puts k[1]
+  #puts v
+  
+  puts a = Record.all(:sha1 => k[0]).first.inspect #k[0].to_s).inspect
+  a.filepath = k[1]
+  #puts a.inspect
+  #a.update(:filepath => k)
 end
+
 
 
 #nsrlmfg = File.open(ARGV[2], 'r')
